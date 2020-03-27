e037403f049c027f4a2a34faa011ff4df853b2f2
David Luebeck <davidluebeck@gmx.net>
2020-03-27 20:16:21 +0100

    Updated blog models.py (fixing DateTimeField)

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField()
     body = models.TextField(max_length=400)
     image = models.ImageField(upload_to='images/')
